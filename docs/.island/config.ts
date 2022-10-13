c2e8aec13e07ffee96634c69f5dc5e584ce77b31
Przeblysk <przeblysk@163.com>
2022-10-13 23:48:27 +0800

    init

---
+++ docs/.island/config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'islandjs'
+
+const getSidebar = () => ({
+  '/note': [
+    {
+      text: '随记随笔',
+      items: [{ text: '简介', link: '/note/home' }],
+    },
+    {
+      text: '博文翻译',
+      items: [{ text: '现代网络浏览器概览-1', link: '/translator/1' }],
+    },
+  ],
+})
+
+const getNav = () => [
+  {
+    text: '首页',
+    link: '/',
+    activeMatch: '^/$|^/',
+  },
+]
+
+export default defineConfig({
+  title: "Przeblysk's Note",
+  icon: '/OSLO.png',
+  themeConfig: {
+    lang: 'zh',
+    locales: {
+      '/zh/': {
+        lang: 'zh',
+        label: '简体中文',
+        selectText: '语言',
+        ariaLabel: '语言',
+        lastUpdatedText: '上次更新',
+        title: "Przeblysk's Note",
+        outlineTitle: '目录',
+        prevPageText: '上一页',
+        nextPageText: '下一页',
+        editLink: {
+          pattern: 'https://github.com/Przeblysk/note/tree/master/docs/:path',
+          text: '📝 在 GitHub 上编辑此页',
+        },
+        nav: getNav(),
+        sidebar: getSidebar(),
+      },
+    },
+    footer: {
+      message: 'Powered by Island.js',
+      copyright: `Copyright © ${new Date().getFullYear()}-present Przeblysk`,
+    },
+  },
+})
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+pageType: home
+
+hero:
+  name: Przeblysk's Note
+  text: 随便写写
+  tagline: 想到什么就写什么，乱七八糟的胡写一通
+  image:
+    src: /OSLO.svg
+    alt: OSLO
+  actions:
+    - theme: brand
+      text: Take a look
+      link: /note/home
+    - theme: alt
+      text: GitHub
+      link: https://github.com/Przeblysk
+features:
+  - title: 一个喜欢做饭的程序猿
+    details: A program ape who likes to cook
+    icon: 🍥
+  - title: 也喜欢摄影
+    details: Also like photography
+    icon: 📷
+  - title: 还有一只可爱的猫
+    details: And a cute cat
+    icon: 🐈
+---
