bfed7fc1efff7a336a711bc45d8cc7217cebfb43
Przeblysk <przeblysk@163.com>
2022-10-27 01:51:52 +0800

    fix: 格式错误

--- docs/note/NumberSelect-1.jsx
+++ docs/note/NumberSelect-1.jsx
@@ -1,39 +1,39 @@
-import React, { useState, useEffect  } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const NumberSelect = () => {
   const [value, setValue] = useState(0)
-  const [message, setMessage = useState('')
+  const [message, setMessage] = useState('')
 
   const onClickMinus = () => {
-    if(value > 0) {
-      setValue(value -1)
+    if (value > 0) {
+      setValue(value - 1)
       setMessage('')
-    }else {
+    } else {
       setMessage('不能再减小啦～')
     }
   }
 
   const onClickPlus = () => {
-    if(value< 10) {
-      setValue(value +1)
+    if (value < 10) {
+      setValue(value + 1)
       setMessage('')
-    }else {
+    } else {
       setMessage('已经最多了噢～')
     }
   }
 
   return (
-    <div className="quantity-selector">
-      <button onClick={onClickMinus} className="button">
+    <div className='quantity-selector'>
+      <button onClick={onClickMinus} className='button'>
         -
       </button>
-      <div className="number">{value}</div>
-      <button onClick={onClickPlus} className="button">
+      <div className='number'>{value}</div>
+      <button onClick={onClickPlus} className='button'>
         +
       </button>
-      <div className="message">{message}</div>
+      <div className='message'>{message}</div>
     </div>
-  );
+  )
 }
 
 export default NumberSelect
